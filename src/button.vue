416099608175aaba8fd77804518ec12fe35bba21
jialili <1663773489@qq.com>
2019-12-30 15:32:49 +0800

    加入parcel

---
+++ src/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="g-button">按钮</button>
+</template>
+<script>
+export default {};
+</script>
+<style  scoped>
+.g-button {
+  font-size: var(--font-size);
+  height: var(--font-height);
+  padding: 0 1em;
+  border-radius: var(--boder-radius);
+  border: 1px solid var(--boder-color);
+  background: var(--button-bg);
+}
+</style>
