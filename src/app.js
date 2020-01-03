63bc96173ce0a73533ad6dafd930e887a64c3276
jialili <1663773489@qq.com>
2020-01-03 11:33:55 +0800

    去掉app.js中的测试用例

--- src/app.js
+++ src/app.js
@@ -6,22 +6,22 @@ new Vue({
     el: '#app'
 })
 //单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect = chai.expect
-{
-    const Constructor = Vue.extend(Button)
-    const button = new Constructor({
-        propsData: {
-            icon: 'settings'
-        }
-    })
-    button.$mount('#test')
-    let useElement = button.$el.querySelector('use')
-    console.log(useElement)
-    let href = useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
-    button.$el.remove()
-    button.$destroy()
-}
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect = chai.expect
+// {
+//     const Constructor = Vue.extend(Button)
+//     const button = new Constructor({
+//         propsData: {
+//             icon: 'settings'
+//         }
+//     })
+//     button.$mount('#test')
+//     let useElement = button.$el.querySelector('use')
+//     console.log(useElement)
+//     let href = useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-settings')
+//     button.$el.remove()
+//     button.$destroy()
+// }
